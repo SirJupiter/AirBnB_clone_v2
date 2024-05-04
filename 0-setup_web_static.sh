dc056305ab30f228e0529535e1301800d8950e9d
SirJupiter <jupitertoby@gmail.com>
2024-05-04 19:04:26 +0100

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -58,11 +58,14 @@ sudo chown -R ubuntu:ubuntu /data/*
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
 # Use alias inside your Nginx configuration
 nginx_config_file="/etc/nginx/sites-available/default"
-new_config="\n\
-	location /hbnb_static {\n\t\t\
-		alias /data/web_static/current/;\n\t\
-	}\n\n\
-"
+new_config=$(cat <<EOF
+
+        location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+
+EOF
+)
 
 if ! sudo sed -i "/server_name _;/a $new_config" "$nginx_config_file"; then
 	echo -e "\nxxxx    Error: Configuration unsuccessful    xxxx\n"
