9ef431b18ea1664a28bb4f9f4ce06982138d5e94
SirJupiter <jupitertoby@gmail.com>
2024-05-04 17:40:16 +0100

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -41,14 +41,19 @@ sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 
 # Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-echo "Simple content" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html>
+	<head>
+  	</head>
+  	<body>
+    	Holberton School
+  	</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
-sudo chown -R "$USER":"$USER" /data/*
-sudo chmod -R 755 /data/
+sudo chown -R ubuntu:ubuntu /data/*
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
 # Use alias inside your Nginx configuration
