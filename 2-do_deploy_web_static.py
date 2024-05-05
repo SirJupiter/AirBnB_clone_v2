5fd0fcaa98f8e435dafd26b0b6f6fef2237b8dac
SirJupiter <jupitertoby@gmail.com>
2024-05-05 23:02:56 +0100

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""script (based on the file 1-pack_web_static.py)
-distributes an archive to your web servers, using the function do_deploy"""
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-import os
-from fabric.api import run, put, env
-
-# Set Fabric environment variables
-env.hosts = ['54.145.81.146', '18.207.234.98']  # servers addresses
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to my web servers
+    """Distributes an archive to a web server.
 
+    Args:
+        archive_path (str): The path of the archive to distribute.
     Returns:
-        True if all operations executed correctly
-        False otherwise
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    if not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
