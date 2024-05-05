4e9bf5fcf8dc75d77ab179f951e22b765e06a7eb
SirJupiter <jupitertoby@gmail.com>
2024-05-05 22:43:39 +0100

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,8 +3,7 @@
 distributes an archive to your web servers, using the function do_deploy"""
 
 import os
-from datetime import datetime
-from fabric.api import local, run, put, env
+from fabric.api import run, put, env
 
 # Set Fabric environment variables
 env.hosts = ['54.145.81.146', '18.207.234.98']  # servers addresses
@@ -23,31 +22,17 @@ def do_deploy(archive_path):
         return False
 
     try:
-        time = datetime.now()
-        year = time.year
-        month = time.month
-        day = time.day
-        hour = time.hour
-        minute = time.minute
-        sec = time.second
-        darchive = f"web_static_{year}{month}{day}{hour}{minute}{sec}.tgz"
-
-        local('mkdir -p versions')
-        local(f'tar -cvzf versions/{darchive} web_static')
-
-        _ = put(f"versions/{darchive}", "/tmp/")
-
-        new_path = f"/data/web_static/releases/{darchive[:-4]}"
-
-        run(f"mkdir -p {new_path}")
-
-        _ = run(f"tar -xvzf /tmp/{darchive} -C {new_path}")
-        run(f"rm /tmp/{darchive}")
-
-        run("rm -rf /data/web_static/current")
-        run(f"ln -s {new_path} /data/web_static/current")
-
-        print("New version deployed!")
+        the_file = archive_path.split("/")[-1]
+        no_ext = the_file.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p {path}{no_ext}/')
+        run(f'tar -xzf /tmp/{the_file} -C {path}{no_ext}/')
+        run(f'rm /tmp/{the_file}')
+        run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
+        run(f'rm -rf {path}{no_ext}/web_static')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s {path}{no_ext}/ /data/web_static/current')
         return True
     except Exception:
         return False
