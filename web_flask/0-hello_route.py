5fefb3f779261b9613be42ec7b28d0957d733802
SirJupiter <jupitertoby@gmail.com>
2024-05-18 21:35:44 +0100

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-"""This module imports app from __init__.py
-app is an instance of the Flask object with "__name__ passed to it
-"""
+"""This module starts a Flask web application"""
 
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def route():
+    """Function displays a web page eith a message
+
+    Return:
+        "Hello HBNB!"
+    """
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
