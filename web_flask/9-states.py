43ea7c3f5b918b4292b8910ca843a4d14bec6653
SirJupiter <jupitertoby@gmail.com>
2024-05-20 15:20:25 +0100

    10. States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def route(state_id):
+    """Displays states and cities listed in alphabetical order
+
+    Return:
+        render_template 9-states.html
+    """
+    states = storage.all(State)
+
+    if state_id:
+        states = states.values()
+        for state in states:
+            if state.id == state_id:
+                return render_template('9-states.html', state=state)
+    # return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Function closes the storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
